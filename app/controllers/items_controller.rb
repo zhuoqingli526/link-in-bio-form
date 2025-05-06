0a2bb613b1a624f446be7e1459dc3e90066a85aa
zhuoqingli <143445503+zhuoqingli526@users.noreply.github.com>
2025-05-06 07:44:51 +0000

    finish

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,21 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+
+  def display_form
+
+    render({ :template => "item_templates/backdoor_form" })
+  end
+
+  def create_item
+    i = Item.new
+    i.link_url = params.fetch("link_url_param")
+    i.link_description = params.fetch("link_description_param")
+    i.thumbnail_url = params.fetch("thumbnail_url_param")
+    i.save
+
+
+    redirect_to("/")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "items", :action => "index" })
+
+  get("/backdoor", { :controller => "items", :action => "display_form" })
+
+  post("/make_a_new_item", { :controller => "items", :action => "create_item" } )
 end
